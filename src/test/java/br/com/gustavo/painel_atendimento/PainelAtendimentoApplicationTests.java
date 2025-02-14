33bf2739a86a0dd8cb03f5ddbd82b5d122f634ff
Gustavo Caçador <gustavoirl456@outlook.com>
2025-02-14 16:17:59 -0300

    projeto criado

---
+++ src/test/java/br/com/gustavo/painel_atendimento/PainelAtendimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.painel_atendimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainelAtendimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
